4839fa8167eff6ec9b854b30539699573f38707d
Jeff Ramnani <jefframnani@yahoo.com>
2010-08-12 22:05:53 -0500

    Refactor Connect() to more idiomatic Dial().
    Connection() now holds the FTP control connection.

--- client.go
+++ client.go
@@ -1,63 +1,57 @@
 package ftp
 
 import (
+  "io"
   "net"
   "os"
   "strings"
 )
 
+// Knows the control connection where commands are sent to the server.
 type Connection struct {
-  host string
-  user string
-  password string
+  control io.ReadWriteCloser
 }
 
 var CRLF = "\r\n"
 
-func (c *Connection) Login() os.Error {
-  conn, err := net.Dial("tcp", "", c.host)
-  if err != nil {
-    return err
+func (c *Connection) cmd() {
+}
+
+// Log into a FTP server using username and password.
+func (c *Connection) Login(user string, password string) os.Error {
+  if user == "" {
+    return os.NewError("FTP Connection Error: User can not be blank!")
   }
-  defer conn.Close()
-  _, err = conn.Write([]byte("USER " + c.user + CRLF))
-  _, err = conn.Write([]byte("PASS " + c.password + CRLF))
+  if password == "" {
+    return os.NewError("FTP Connection Error: Password can not be blank!")
+  }
+  _, err := c.control.Write([]byte("USER " + user + CRLF))
+  _, err = c.control.Write([]byte("PASS " + password + CRLF))
   if err != nil {
     return err
   }
   return nil
 }
 
-func newConnection(host string, user string, password string) (*Connection) {
-  return &Connection{host, user, password}
-}
-
-// Reused from src/pkg/http/client.go
-// Given a string of the form "host", "host:port", or "[ipv6::address]:port",
-// return true if the string includes a port.
-func hasPort(s string) bool { return strings.LastIndex(s, ":") > strings.LastIndex(s, "]") }
-
-func Connect(host string, user string, password string) (*Connection, os.Error) {
+// Dials up a remote FTP server.
+// host should be in the form of address:port e.g. myserver:21 or myserver:ftp
+func Dial(host string) (*Connection, os.Error) {
   if host == "" {
     return nil, os.NewError("FTP Connection Error: Host can not be blank!")
   }
   if !hasPort(host) {
     return nil, os.NewError("FTP Connection Error: Host must have a port! e.g. host:21")
   }
-  if user == "" {
-    return nil, os.NewError("FTP Connection Error: User can not be blank!")
+  conn, err := net.Dial("tcp", "", host)
+  if err != nil {
+    return nil, err
   }
-  return newConnection(host, user, password), nil
+  return &Connection{conn}, nil
 }
 
 
-func (c Connection) String() string {
-  // Only print the password if the user is anonymous.
-  if c.user == "anonymous" {
-    return "FTP Connection " + "HOST: " + c.host + " USER: " + c.user + " PASS: " +c.password
-  } else {
-    return "FTP Connection " + "HOST: " + c.host + " USER: " + c.user + " PASS: [secret]"
-  }
-  return "The compiler made me do this."
-}
+// Reused from src/pkg/http/client.go
+// Given a string of the form "host", "host:port", or "[ipv6::address]:port",
+// return true if the string includes a port.
+func hasPort(s string) bool { return strings.LastIndex(s, ":") > strings.LastIndex(s, "]") }
 
--- client_test.go
+++ client_test.go
@@ -5,32 +5,33 @@ import (
   "testing"
 )
 
-func TestConnect(t *testing.T) {
-  _, err := Connect("localhost:21", "anonymous", "anonymous")
+func TestDial(t *testing.T) {
+  _, err := Dial("")
+  if err == nil {
+    t.Error("Dial() should have an error on blank 'host'!")
+  }
+  _, err = Dial("localhost:21")
   if err != nil {
     t.Error(err)
-  }
-}
-
-func TestConnectErrors(t *testing.T) {
-  _, err := Connect("", "anonymous", "anonymous")
-  if err == nil {
-    t.Error("Connect() should have an error on blank 'host'!")
-  }
-  _, err = Connect("localhost:21", "", "anonymous")
-  if err == nil {
-    t.Error("Connect() should have an error on blank 'user'!")
   }
 }
 
 func TestLogin(t *testing.T) {
-  conn, err := Connect("localhost:21", "anonymous", "anonymous")
+  conn, err := Dial("localhost:21")
   if err != nil {
     t.Fatal(err)
   }
-  err = conn.Login()
+  err = conn.Login("anonymous", "anonymous@")
   if err != nil {
     t.Error(err)
+  }
+  err = conn.Login("", "anonymous@")
+  if err == nil {
+    t.Error("Login() should have an error on blank 'user'!")
+  }
+  err = conn.Login("anonymous", "")
+  if err == nil {
+    t.Error("Login() should have an error on blank 'password'!")
   }
 }
 
