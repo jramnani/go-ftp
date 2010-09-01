bef28dd608acd526fe32c2c4f8e900d0e16813c5
Jeff Ramnani <jefframnani@yahoo.com>
2010-08-31 23:49:02 -0500

    DownloadFile() now downloads a file.
    Needs refactoring, but it's a start.

--- client.go
+++ client.go
@@ -1,7 +1,7 @@
 package ftp
 
 import (
-  /*"fmt"*/
+  "fmt"
   "io"
   "net"
   "os"
@@ -13,9 +13,13 @@ import (
 // Knows the control connection where commands are sent to the server.
 type Connection struct {
   control io.ReadWriteCloser
+  hostname string
 }
 
 var CRLF = "\r\n"
+var ASCII = "A"
+var BINARY = "I"
+var IMAGE = "I" //Synonymous with "Binary"
 
 // Dials up a remote FTP server.
 // host should be in the form of address:port e.g. myserver:21 or myserver:ftp
@@ -31,7 +35,22 @@ func Dial(host string) (*Connection, os.Error) {
   if err != nil {
     return nil, err
   }
-  return &Connection{conn}, nil
+  // Upon connect, most servers respond with a welcome message.
+  // The welcome message contains a status code, just like any other command.
+  // TODO: Handle servers with no welcome message.
+  welcomeMsg := make([]byte, 1024)
+  _, err = conn.Read(welcomeMsg)
+  if err != nil {
+    return nil, os.NewError("Couldn't read the server's initital connection information. Error: " + err.String())
+  }
+  code, err := strconv.Atoui(string(welcomeMsg[0:3]))
+  err = checkResponseCode(2, code)
+  if err != nil {
+    return nil, os.NewError("Couldn't read the server's Welcome Message. Error: " + err.String())
+  }
+  // This doesn't work for IPv6 addresses.
+  hostParts := strings.Split(host, ":", 2)
+  return &Connection{conn, hostParts[0]}, nil
 }
 
 // Executes an FTP command.
@@ -77,6 +96,97 @@ func (c *Connection) Login(user string, password string) os.Error {
   return nil
 }
 
+// Download a file from a remote server.
+// Assumes only passive FTP connections for now.
+func (c *Connection) DownloadFile(src, dest, mode string) os.Error {
+  // Use PASV to set up the data port.
+  pasvCode, pasvLine, err := c.Cmd("PASV", "")
+  if err != nil {
+    return err
+  }
+  pasvErr := checkResponseCode(2, pasvCode)
+  if pasvErr != nil {
+    msg := fmt.Sprintf("Cannot set PASV. Error: %v", pasvErr)
+    return os.NewError(msg)
+  }
+  dataPort, err := extractDataPort(pasvLine)
+  /*_, err = extractDataPort(pasvLine)*/
+  if err != nil {
+    return err
+  }
+
+  // Set the TYPE (ASCII or Binary)
+  typeCode, typeLine, err := c.Cmd("TYPE", mode)
+  if err != nil {
+    return err
+  }
+  typeErr := checkResponseCode(2, typeCode)
+  if typeErr != nil {
+    msg := fmt.Sprintf("Cannot set TYPE. Error: '%v'. Line: '%v'", typeErr, typeLine)
+    return os.NewError(msg)
+  }
+
+  // Can't use Cmd() for RETR because it doesn't return until *after* you've
+  // downloaded the requested file.
+  command := []byte("RETR " + src + CRLF)
+  _, err = c.control.Write(command)
+  if err != nil {
+    return err
+  }
+
+  // Open connection to remote data port.
+  remoteConnectString := c.hostname + ":" + fmt.Sprintf("%d", dataPort)
+  download_conn, err := net.Dial("tcp", "", remoteConnectString)
+  defer download_conn.Close()
+  if err != nil {
+    msg := fmt.Sprintf("Couldn't connect to server's remote data port. Error: %v", err)
+    return os.NewError(msg)
+  }
+
+  // Set up the destination file
+  var filePerms uint32 = 0664
+  destFile, err := os.Open(dest, os.O_CREAT|os.O_WRONLY, filePerms)
+  defer destFile.Close()
+  if err != nil {
+    msg := fmt.Sprintf("Cannot open destination file, '%s'. %v", dest, err)
+    return os.NewError(msg)
+  }
+
+  // Buffer for downloading and writing to file
+  bufLen := 1024
+  buf := make([]byte, bufLen)
+
+  // Read from the server and write the contents to a file
+  for {
+    bytesRead, readErr := download_conn.Read(buf)
+    if bytesRead > 0 {
+      _, err := destFile.Write(buf[0:bytesRead])
+      if err != nil {
+        msg := fmt.Sprintf("Coudn't write to file, '%s'. Error: %v", dest, err)
+        return os.NewError(msg)
+      }
+    }
+    if readErr == os.EOF {
+      break
+    }
+    if readErr != nil {
+      return readErr
+    }
+  }
+  return nil
+}
+
+// Given an prefix, does the response code match the expected code?
+func checkResponseCode(expectCode, code uint) os.Error {
+  if 1 <= expectCode && expectCode < 10 && code/100 != expectCode ||
+    10 <= expectCode && expectCode < 100 && code/10 != expectCode ||
+      100 <= expectCode && expectCode < 1000 && code != expectCode {
+        msg := fmt.Sprintf("Bad response from server. Expected: %d, Got: %d", expectCode, code)
+        return os.NewError(msg)
+      }
+  return nil
+}
+
 // Interrogate a server response for the remote port on which to connect.
 // Returns the port to be used as the data port for transfers.
 func extractDataPort(line string) (port uint, err os.Error) {
@@ -87,6 +197,10 @@ func extractDataPort(line string) (port uint, err os.Error) {
     return 0, err
   }
   match := re.FindStringSubmatch(line)
+  if len(match) == 0 {
+    msg := "Cannot find data port in server output: " + line
+    return 0, os.NewError(msg)
+  }
   octets := strings.Split(match[1], ",", 2)
   firstOctet, _ := strconv.Atoui(octets[0])
   secondOctet, _ := strconv.Atoui(octets[1])
